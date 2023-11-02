e3ed79e2dd18947348440c5960d8bd3cf1fc973d
Ayeshmantha Madara Wijayagunethilake <ayesh99747@gmail.com>
2023-11-02 10:44:00 +0000

    Created the top header.

--- app/src/main/java/com/example/jetpacktipapp/MainActivity.kt
+++ app/src/main/java/com/example/jetpacktipapp/MainActivity.kt
@@ -3,44 +3,85 @@ package com.example.jetpacktipapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetpacktipapp.ui.theme.JetpackTipAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackTipAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+
+            MyApp {
+                TopHeader()
             }
         }
     }
 }
 
+/*
+    We want to decouple the app by using a container function.
+    The JetTipAppTheme is a composable function.
+    The MyApp function is going to take in a composable as a parameter.
+ */
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+private fun MyApp(content: @Composable () -> Unit) {
+    JetpackTipAppTheme {
+        Surface(
+            color = MaterialTheme.colorScheme.background
+        ) {
+            content()
+        }
+    }
 }
+
+@Preview(showBackground = true)
+@Composable
+fun TopHeader(totalPerPerson: Double = 0.0){
+    Surface (
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(150.dp)
+            .clip(shape = CircleShape.copy(all = CornerSize(12.dp))),
+        color = Color(0xFFe9d7f7)
+    ) {
+        Column (
+            modifier = Modifier.padding(12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+        ) {
+            val total = "%.2f".format(totalPerPerson)
+            Text(text = "Total Per Person", style = MaterialTheme.typography.headlineMedium)
+            Text(text = "$$total", style = MaterialTheme.typography.headlineLarge, fontWeight = FontWeight.ExtraBold)
+        }
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     JetpackTipAppTheme {
-        Greeting("Android")
+        MyApp {
+            Text(text = "Hello Again")
+        }
     }
 }
